8c9e84ec0cadd89a7d5bbe5ac93d4d167ecef306
Kinny <53189297+22TonyFStark@users.noreply.github.com>
2022-06-17 14:19:26 +0800

    Add files via upload

---
+++ QSAttn/test.sh
@@ -0,0 +1,8 @@
+python test.py \
+--dataroot=../../data/selfie2anime \
+--name=anime_QSA \
+--QS_mode=global \
+--gpu_ids 2 \
+--load_size 128 \
+--crop_size 128 \
+--batch_size 16
---
+++ QSAttn/train.sh
@@ -0,0 +1,8 @@
+python train.py \
+--dataroot=../../data/selfie2anime \
+--name=anime_QSA \
+--QS_mode=global \
+--gpu_ids 3 \
+--load_size 128 \
+--crop_size 128 \
+--batch_size 16
